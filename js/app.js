65b85040e9c4624b3dcbb703df32257e62f272c4
Michael Hemingway <mikemingway@gmail.com>
2016-11-24 07:19:46 -0500

    Arthem Branding in console

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
  *
  * Michael Hemingway 2016 */
 
+/*global console*/
+
 (function () {
 	'use strict';
 
@@ -10,5 +12,19 @@
 	document.addEventListener('DOMContentLoaded', function (event) {});
 
 	/*  Event fires when all images, iframes and content is loaded and rendered/renderable. This is useful to trigger the release of a preloader  and run functions that manipulate images / content. */
-	window.addEventListener("load", function (event) {});
+	window.addEventListener("load", function (event) {
+
+
+
+		// Our company signature :P
+		var t = "color: #bada55; background: #222";
+		console.log("%c  █████╗ ██████╗ ████████╗██╗  ██╗███████╗███╗   ███╗     ██████╗ ██████╗    ", t);
+		console.log("%c ██╔══██╗██╔══██╗╚══██╔══╝██║  ██║██╔════╝████╗ ████║    ██╔════╝██╔═══██╗   ", t);
+		console.log("%c ███████║██████╔╝   ██║   ███████║█████╗  ██╔████╔██║    ██║     ██║   ██║   ", t);
+		console.log("%c ██╔══██║██╔══██╗   ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║    ██║     ██║   ██║   ", t);
+		console.log("%c ██║  ██║██║  ██║   ██║   ██║  ██║███████╗██║ ╚═╝ ██║    ╚██████╗╚██████╔╝██╗", t);
+		console.log("%c ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝     ╚═════╝ ╚═════╝ ╚═╝", t);
+		console.log("Site developed with love by Arthem.");
+		console.log("https://arthem.co/ ❤");
+	});
 }()); // End self-invoking function (closure)
